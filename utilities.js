10af262f098f00e85213e5e67255d29a1e47f12c
hsiangting49 <157249687+hsiangting49@users.noreply.github.com>
2024-04-13 17:18:12 -0400

    Initial commit

---
+++ utilities.js
@@ -0,0 +1,17 @@
+
+
+/*
+  function getRandomName() {
+    const namelist = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+    return {
+        get name() {
+            return namelist[Math.floor(Math.random() * namelist.length)];
+        },
+    }
+  }
+
+<div x-data="getRandomName">
+    <button @click="name = name">Click me</button>
+    <h1 x-text="name"></h1>
+</div>
+ */   
